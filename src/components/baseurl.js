258fa4dd03e599659b4aa14a19dd3bee66e20db8
GreatAdebayo <adebayogreat997@gmail.com>
2022-10-06 10:37:57 +0100

    first commit

---
+++ src/components/baseurl.js
@@ -0,0 +1,9 @@
+export const Baseurl = 'http://127.0.0.1:8000/api/';
+
+export const Mediaurl = 'http://127.0.0.1:8000/storage/coursematerials/';
+
+
+// export const Baseurl = 'https://fromhome.adebayogreat.dev/backend/api/';
+
+// export const Mediaurl = 'https://fromhome.adebayogreat.dev/backend/public/storage/coursematerials/';
+
